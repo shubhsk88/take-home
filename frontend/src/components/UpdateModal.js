0ff218435a9b1dddb1986a9ca4397114c2dc4b76
shubhsk88 <shubham88singh@gmail.com>
2021-03-01 21:03:32 +0530

    Completing the update user modal

--- frontend/src/components/UpdateModal.js
+++ frontend/src/components/UpdateModal.js
@@ -1,3 +1,4 @@
+import { gql, useMutation } from "@apollo/client";
 import {
   Modal,
   ModalOverlay,
@@ -11,13 +12,68 @@ import {
   FormLabel,
   Input,
   VStack,
+  useModal,
+  useToast,
 } from "@chakra-ui/react";
+import { TOTAL_PAGE_QUERY } from "../App";
 import useForm from "../utils/useForm";
+
+const UPDATE_USER_MUTATION = gql`
+  mutation updateUser(
+    $id: ID!
+    $name: String!
+    $sales: Float!
+    $amount: Float!
+    $quantity: Int!
+    $company: String!
+  ) {
+    updateUser(
+      id: $id
+      data: {
+        name: $name
+        amount: $amount
+        quantity: $quantity
+        company: $company
+        sales: $sales
+      }
+    ) {
+      id
+      name
+    }
+  }
+`;
 
 const UpdateModal = ({ row }) => {
   const { inputs, handleChange } = useForm(row);
+  const toast = useToast();
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const [updateUser] = useMutation(UPDATE_USER_MUTATION, {
+    variables: inputs,
+    refetchQueries: [{ query: TOTAL_PAGE_QUERY }],
+  });
 
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      await updateUser();
+      toast({
+        title: "Success",
+        description: "User Updated Successfully",
+        status: "success",
+        duration: 1000,
+        isClosable: true,
+      });
+    } catch (error) {
+      toast({
+        title: "Error",
+        description: error.message,
+        status: "error",
+        duration: 1000,
+        isClosable: true,
+      });
+    }
+  };
   return (
     <>
       <Button variant="link" size="md" color="purple.500" onClick={onOpen}>
@@ -30,7 +86,7 @@ const UpdateModal = ({ row }) => {
           <ModalHeader>Edit Profile</ModalHeader>
           <ModalCloseButton />
           <ModalBody>
-            <VStack as="form" spacing={4}>
+            <VStack as="form" spacing={4} onSubmit={onSubmit}>
               <FormControl id="name" isRequired>
                 <FormLabel>Name</FormLabel>
                 <Input
@@ -81,7 +137,13 @@ const UpdateModal = ({ row }) => {
                   onChange={handleChange}
                 />
               </FormControl>
-              <Button my={4} w="80%" onClick={onClose} colorScheme="teal">
+              <Button
+                my={4}
+                w="80%"
+                type="submit"
+                onClick={onClose}
+                colorScheme="teal"
+              >
                 Update
               </Button>
             </VStack>
